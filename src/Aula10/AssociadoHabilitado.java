87eb894866ab17c68a3eabcf2458e5c81e862b0a
Mateus Benites Dias <imattdias@gmail.com>
2021-11-10 09:09:08 -0300

    POO - Aula 10 - Atividade Prof

---
+++ src/Aula10/AssociadoHabilitado.java
@@ -0,0 +1,20 @@
+package Aula10;
+
+public class AssociadoHabilitado extends Associado {
+    private double custoPiscina;
+    private boolean habilitado;
+
+    public AssociadoHabilitado(String numero, String nome, double valorMensal, String atividade, double custoPiscina){
+        super(numero, nome, valorMensal, atividade);
+        this.custoPiscina = custoPiscina;
+    }
+
+    @Override
+    public double custoMensal() {
+        if (this.habilitado) {
+            return super.custoMensal() + this.custoPiscina;
+        } else {
+            return super.custoMensal();
+        }
+    }
+}
